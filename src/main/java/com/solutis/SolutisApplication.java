21d34f25b5d7588a70b0d9a0ec7d1fa49bfe244c
Lucas Bassetto <lucasfbassetto@gmail.com>
2023-08-17 13:01:50 -0300

    Project created

---
+++ src/main/java/com/solutis/SolutisApplication.java
@@ -0,0 +1,13 @@
+package com.solutis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolutisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solutis/SolutisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solutis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
